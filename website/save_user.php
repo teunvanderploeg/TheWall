bd3448c13fdfadfbb101c12096382c8562c7a273
Teun van der Ploeg <29253@ma-web.nl>
2020-03-20 12:56:11 +0100

    check
    het script check op usernam en email of die all in de database zitten

--- website/save_user.php
+++ website/save_user.php
@@ -9,21 +9,55 @@ $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 $wachtwoord = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT); 
 
 
+echo 'gebruikersnaam: ';
+echo $gebruikersnaam;
+echo '<br>';
+echo 'Email: ';
+echo $email;
+echo '<br>';
 
-    // $stmtt = $pdo->prepare("SELECT 1 FROM gebruikers WHERE email= $email");
 
-            
+
+
+    $stmt_e = $dbConnect->prepare("SELECT * FROM gebruikers WHERE email= '$email'");
+
+    $params = [
+       'email' => $email
+    ];
+
+    $stmt_e->execute($params);
+
+    if($stmt_e->rowCount() > 0){
+      echo "Deze email bestaat al";
+      exit;
+    }else{
+
+
+        $stmt_g = $dbConnect->prepare("SELECT * FROM gebruikers WHERE gebruikersnaam = '$gebruikersnaam' ";);
+
+        $paramss = [
+           'gebruikersnaam' => $gebruikersnaam
+        ];
+    
+        $stmt_g->execute($paramss);
+    
+        if($stmt_g->rowCount() > 0){
+          echo "Deze gebruikersnaam bestaat al";
+          exit;
+        }else{       
     
     $stmt = $dbConnect->prepare(
         "INSERT INTO gebruikers (gebruikersnaam, email, wachtwoord)
         VALUES (:gebruikersnaam, :email, :wachtwoord)");
+
         $stmt->bindParam(':gebruikersnaam', $gebruikersnaam);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':wachtwoord', $wachtwoord);
 
         $stmt->execute();
-
+        echo "Dit account bestaat al";
         header('Location: index.php');
         exit;
-     
+    }
+    }
 ?>
