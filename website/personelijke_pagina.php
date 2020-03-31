05728084772260ebc96f409be9adb5445c1011aa
Teun van der Ploeg <29253@ma-web.nl>
2020-03-31 14:21:00 +0200

    delete knop
    delete knop probeeren mooi te maken

--- website/personelijke_pagina.php
+++ website/personelijke_pagina.php
@@ -18,7 +18,9 @@ $track = $statement->fetch();
     <meta name="description" content="thewall" />
     <meta name="keywords" content="thewall" />
     <meta name="author" content="Teun en Romer" />
+    <script src="https://kit.fontawesome.com/af1b884290.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="style/index.css" />
+    <link rel="stylesheet" type="text/css" href="style/personelijk.css" />
     <title>Welkom <?php if ( ! isset( $_SESSION['user_id'] ) ) {echo "";}else{echo $_SESSION['gebruikersnaam'];}?> bij TheWall</title>
   </head>
   <body>
@@ -28,8 +30,7 @@ $track = $statement->fetch();
       <div class="track" id="track-<?php echo $afbeeldingen['id'] ?>">
         <img src="<?php echo $afbeeldingen['afbeelding'] ?>" alt="<?php echo $afbeeldingen['titel'] ?>"/>
     <?php if ( $_SESSION['user_id'] === $user_id )  { ?>
-    <a href='delete.php?id=<?php echo $afbeeldingen['id']; ?>'><button class='delete'>Delete</button></a>
-    <!-- <a href='edit.php?id=<?php //echo $afspeelijst['id']; ?>'><button class='edit'>edit</button></a> -->
+    <a class="deletelink" href='delete.php?id=<?php echo $afbeeldingen['id']; ?>'><i class="fas fa-times deletebutton"></i></a>
     <?php } ?>
       </div>
     <?php endforeach ?>
